c72a4a54c418afadf2320ce6b630a051eade46e0
mutierehman <mzmutirehman@gmail.com>
2024-09-18 12:45:41 +0500

    2 Files Added

---
+++ Arithmetic Operation.py
@@ -0,0 +1,72 @@
+##There are various operation performed with python data types( int, float, str, bool)
+
+# 1) Integer Operation (int)
+# You can perfrom arithematic operation like addition, Subtraction, Multiplication, Devision
+
+
+print("*************Integer Operation**************")
+a=int(input('Enter Value of a: '))
+b=int(input('Enter Value of b: '))
+
+# Addition
+add_result=a+b
+print('Addition: a+b=',add_result)
+
+#Subtraction
+sub_result=a-b
+print('Subtraction: a-b=',sub_result)
+
+#Multiplication
+mult_result=a*b
+print('Multiplication: a*b=',mult_result)
+
+#Division
+div_result=a/b
+print('Division: a/b=',div_result)
+
+#Integer Division (floor Division)
+div_int_result=a//b
+print('Integer Division: a//b=',div_int_result)
+
+#Modulus (Remainder)
+modu_result=a%b
+print('Modulus Remainder: a%b=', modu_result)
+
+#Exponention
+pow_result=a**b
+print('Exponentiation: a**b=',pow_result)
+
+# 2 Floating-Point Operation(float)
+# In floating point with decimal number
+
+print("*************Floating-Point Operation**************")
+a=float(input('Enter Value of a: '))
+b=float(input('Enter Value of b: '))
+
+# Addition
+add_result=a+b
+print('Addition: a+b=',add_result)
+
+#Subtraction
+sub_result=a-b
+print('Subtraction: a-b=',sub_result)
+
+#Multiplication
+mult_result=a*b
+print('Multiplication: a*b=',mult_result)
+
+#Division
+div_result=a/b
+print('Division: a/b=',div_result)
+
+#Integer Division (floor Division)
+div_int_result=a//b
+print('Integer Division: a//b=',div_int_result)
+
+#Modulus (Remainder)
+modu_result=a%b
+print('Modulus Remainder: a%b=', modu_result)
+
+#Exponention
+pow_result=a**b
+print('Exponentiation: a**b=',pow_result)
